1d76e67a301214ba203f7faa7d6dbd1cb53e3361
Daniel Kim <daniel.jaewoo.kim@gmail.com>
2017-09-26 21:10:03 -0400

    Add files via upload

--- abPruning.py
+++ abPruning.py
@@ -10,50 +10,63 @@ import time
 import Board
 
 # Depth allowed to run minimax until the evaluation function is used
-minimax_depth = 10
+MAX_NODES = 20
+minimax_nodes = 0
 
 # Checks depth to see if minimax_depth has been reached
-def checkCutOff(currentDepth):
-    if currentDepth >= minimax_depth:
+def checkCutOff(current):
+    global minimax_nodes
+    if current >= MAX_NODES:
+        #print(current)
         return True
     else:
+        print(minimax_nodes)
         return False
 
 # "Max" in the minimax algorithm
-def maximumValue(state, alpha, beta, depth):
+def maximumValue(state, alpha, beta, m):
+    global minimax_nodes
+    
     result = state.check_terminal_state()
     if result:
         if result == "tie":
             return 0
         else:
             return float(result)
-    if checkCutOff(depth):
-        #print("in eval")
+    if checkCutOff(minimax_nodes):
+        #print("in eval max")
         #Evaluation.evaluationFunction(state)
         return 1.0
+    minimax_nodes += 1
+    #state.print_board()
     value = -math.inf
     for child in state.get_children():
-        value = max(value, minimumValue(child, alpha, beta, depth+1))
+        #minimax_nodes += 1
+        value = max(value, minimumValue(child, alpha, beta, m))
         if value >= beta:
             return value
         alpha = max(alpha, value)
     return value
 
 # "Min" in the minimax algorithm
-def minimumValue(state, alpha, beta, depth):
+def minimumValue(state, alpha, beta, m):
+    global minimax_nodes
     result = state.check_terminal_state()
     if result:
         if result == "tie":
             return 0
         else:
             return float(result)
-    if checkCutOff(depth):
+    if checkCutOff(minimax_nodes):
         #print("in eval min")
         #Evaluation.evaluationFunction(state)
         return 1.0
+    minimax_nodes += 1
+    #state.print_board()
     value = math.inf
     for child in state.get_children():
-        value = min(value, maximumValue(child, alpha, beta, depth+1))
+        #minimax_nodes += 1
+        value = min(value, maximumValue(child, alpha, beta, m))
         if value <= alpha:
             return value
         beta = min(beta, value)
@@ -66,7 +79,7 @@ def minimaxABPruning(gameBoard):
     if (gameBoard.number_filled_cells == 1 and
         gameBoard.board["H8"].color == gameBoard.color_next_turn):
         #replace piece
-        return Move
+        return
     best_val = -math.inf
     beta = math.inf
     children = gameBoard.get_children()
@@ -106,7 +119,15 @@ b1 = Board.Board("black")
 for i in range(15):
     b1.make_move("A", str(i+1))
 for i in range(15):
-    b1.make_move("E", str(i+1))
+    b1.make_move("F", str(i+1))
+b1.make_move("B", "3")
+b1.make_move("B", "2")
+b1.make_move("B", "4")
+b1.make_move("B", "8")
+b1.make_move("B", "5")
+b1.make_move("M", "3")
+b1.make_move("B", "6")
+b1.make_move("B", "9")
 ##for i in range(15):
 ##    b1.make_move("B", str(i+1))
 ##for i in range(15):
